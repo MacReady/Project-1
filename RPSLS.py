468a1bf7ed303da683a6c71db875d1a578b05d5b
Stephen <whippetlegs@gmail.com>
2013-04-25 14:19:38 +0000

    First two functions done

--- RPSLS.py
+++ RPSLS.py
@@ -14,42 +14,45 @@
 # helper functions
 
 def number_to_name(number):
+
+    
     # fill in your code below
+
+    if number == 0:
+        return 'rock'
+    elif number == 1:
+        return 'Spock'
+    elif number == 2:
+        return 'paper'
+    elif number == 3:
+        return 'lizard'
+    elif number == 4:
+        return 'scissors'
+    else:
+        return 'This choice is not allowed'
     
     # convert number to a name using if/elif/else
     # don't forget to return the result!
 
-    
 def name_to_number(name):
     # fill in your code below
 
+
+    if name == 'rock':
+        return 0
+    elif name == 'Spock':
+        return 1
+    elif name == 'paper':
+        return 2
+    elif name == 'lizard':
+        return 3
+    elif name == 'scissors':
+        return 4
+    else:
+        return 'This choice is not allowed'
+    
+
     # convert name to number using if/elif/else
     # don't forget to return the result!
-
-
-def rpsls(name): 
-    # fill in your code below
-
-    # convert name to player_number using name_to_number
-
-    # compute random guess for comp_number using random.randrange()
-
-    # compute difference of player_number and comp_number modulo five
-
-	# use if/elif/else to determine winner
-
-    # convert comp_number to name using number_to_name
-    
-    # print results
-
-    
-# test your code
-rpsls("rock")
-rpsls("Spock")
-rpsls("paper")
-rpsls("lizard")
-rpsls("scissors")
-
-# always remember to check your completed program against the grading rubric
 
 
